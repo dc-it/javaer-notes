8144026f12b2b9f5b7d0d45564532a720f3c410e
dc-it <mapcduchao123@gmail.com>
2021-02-03 22:39:35 +0800

    nacos-springboot-config

--- microservice/nacos/sample/nacos-springboot-config/src/test/java/com/dc/nacos/NacosSpringbootConfigApplicationTests.java
+++ microservice/nacos/sample/nacos-springboot-config/src/test/java/com/dc/nacos/NacosSpringbootConfigApplicationTests.java
@@ -1,13 +1,16 @@
 package com.dc.nacos;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class NacosSpringbootConfigApplicationTests {
+public class NacosSpringbootConfigApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }

9ca7b752d16d9b1cc4e5c63b91b5ce6752c18859
duchaoyun <1255604509@qq.com>
2021-02-03 18:05:46 +0800

    aa

---
+++ microservice/nacos/sample/nacos-springboot-config/src/main/java/com/dc/nacos/NacosSpringbootConfigApplication.java
@@ -0,0 +1,13 @@
+package com.dc.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringbootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSpringbootConfigApplication.class, args);
+	}
+
+}
---
+++ microservice/nacos/sample/nacos-springboot-config/src/test/java/com/dc/nacos/NacosSpringbootConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringbootConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

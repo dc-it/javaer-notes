f6e6224ffc8cf03c7e159e14d8fd3dbb6a6bd5f1
dc-it <mapcduchao123@gmail.com>
2021-02-03 22:54:05 +0800

    nacos-springboot-discovery

---
+++ microservice/nacos/sample/nacos-springboot-discovery/src/main/java/com/dc/nacos/NacosSpringbootDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.dc.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringbootDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringbootDiscoveryApplication.class, args);
+    }
+
+}
---
+++ microservice/nacos/sample/nacos-springboot-discovery/src/test/java/com/dc/nacos/NacosSpringbootDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dc.nacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NacosSpringbootDiscoveryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

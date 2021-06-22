53f38e22eadf301bac1705a2af39e31a50e0d5f7
duchaoyun <1255604509@qq.com>
2021-06-22 14:14:56 +0800

    设计模式

--- designpattern/sample/src/main/java/ProxyTest.java
+++ designpattern/sample/src/main/java/ProxyTest.java
@@ -1,8 +1,16 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * 代理模式：静态代理、动态代理
- * 静态代理：
+ * 静态代理：每一个类都有一个委托代理类
+ * 动态代理：JDK动态代理、cglib动态代理
  *
  * @author duchao
  */
@@ -13,8 +21,32 @@ public class ProxyTest {
      */
     @Test
     public void testStaticProxy(){
-        CarProxy carProxy = new CarProxy(new Audi());
-        carProxy.run();
+        AudiProxy audiProxy = new AudiProxy(new Audi());
+        audiProxy.run();
+    }
+
+    /**
+     * JDK动态代理
+     */
+    @Test
+    public void testJdkDynamicProxy(){
+        Car audiCar = (Car) Proxy.newProxyInstance(Audi.class.getClassLoader(), Audi.class.getInterfaces(), new CarJdkDynamicHandler(new Audi()));
+        audiCar.run();
+
+        Car bmwCar = (Car) Proxy.newProxyInstance(BMW.class.getClassLoader(), BMW.class.getInterfaces(), new CarJdkDynamicHandler(new BMW()));
+        bmwCar.run();
+    }
+
+    /**
+     * Cglib动态代理
+     */
+    @Test
+    public void testCglibDynamicProxy(){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Animal.class);
+        enhancer.setCallback(new AnimalCglibDynamicHandler());
+        Animal animal = (Animal) enhancer.create();
+        animal.hello();
     }
 
     public interface Car{
@@ -23,25 +55,72 @@ public class ProxyTest {
 
     public class Audi implements Car{
 
+        public Audi(){
+
+        }
+
         @Override
         public void run() {
             System.out.println("奥迪在飞奔");
         }
+
     }
 
-    public class CarProxy implements Car{
+    public class BMW implements Car{
 
-        private Car car;
+        @Override
+        public void run() {
+            System.out.println("宝马在飞奔");
+        }
 
-        public CarProxy(Car car){
-            this.car = car;
+    }
+
+    public class AudiProxy implements Car{
+
+        private Audi audi;
+
+        public AudiProxy(Audi audi){
+            this.audi = audi;
         }
 
         @Override
         public void run() {
-            System.out.println("车代理执行前...");
-            car.run();
-            System.out.println("车代理执行后...");
+            System.out.println("车启动...");
+            audi.run();
+            System.out.println("车停了...");
+        }
+    }
+
+    public class CarJdkDynamicHandler implements InvocationHandler{
+
+        private Object object;
+
+        public CarJdkDynamicHandler(Object object) {
+            this.object = object;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("车启动...");
+            method.invoke(object,args);
+            System.out.println("车停了...");
+            return null;
+        }
+    }
+
+    static class Animal{
+        void hello(){
+            System.out.println("我是动物");
+        }
+    }
+
+    public class AnimalCglibDynamicHandler implements MethodInterceptor{
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+            System.out.println("before...");
+            Object res = methodProxy.invokeSuper(obj, args);
+            System.out.println("after...");
+            return res;
         }
     }
 

7fc9ceb46f11fdd6b599b00411bf159ad62d3daf
dc-it <mapcduchao123@gmail.com>
2021-06-23 00:02:20 +0800

    IO-NIO

--- java/sample/src/test/java/io/NIOTest.java
+++ java/sample/src/test/java/io/NIOTest.java
@@ -2,6 +2,17 @@ package io;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * NIO  同步非阻塞
  *
@@ -33,11 +44,26 @@ import org.junit.Test;
  */
 public class NIOTest {
 
+    private final static int port = 1111;
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void testIOClient() throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("127.0.0.1",port));
+    }
 
     @Test
-    public void testIOServer(){
-
+    public void testIOServer() throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(port));
+        while (true){
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            socketChannel.write(byteBuffer);
+        }
     }
 
 }

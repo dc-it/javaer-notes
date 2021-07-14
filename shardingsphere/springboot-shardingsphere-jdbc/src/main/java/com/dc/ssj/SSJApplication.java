214eec105614c8a7b556ca5c70fcc0f17fc41531
duchaoyun <1255604509@qq.com>
2021-07-14 22:05:16 +0800

    mysql主从复制、主主复制(双主备)、高可用双机热备、读写分离

---
+++ shardingsphere/springboot-shardingsphere-jdbc/src/main/java/com/dc/ssj/SSJApplication.java
@@ -0,0 +1,13 @@
+package com.dc.ssj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSJApplication.class, args);
+	}
+
+}

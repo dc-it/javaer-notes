c3861f2f84dc350476a436aef149169c32b414d0
duchaoyun <1255604509@qq.com>
2021-07-15 18:37:11 +0800

    shardingsphere

--- shardingsphere/springboot-shardingsphere-jdbc/src/test/java/com/dc/ssj/SSJApplicationTests.java
+++ shardingsphere/springboot-shardingsphere-jdbc/src/test/java/com/dc/ssj/SSJApplicationTests.java
@@ -3,8 +3,14 @@ package com.dc.ssj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 @SpringBootTest
 class SSJApplicationTests {
+
+	@Resource
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
